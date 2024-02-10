9258f2110a0e5a597c34e72d25c7983b05566c35
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-02-10 22:48:34 +0530

    this is master commit

---
+++ src/main/java/com/adarsh/learing/DemoForSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.adarsh.learing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForSpringSecurityApplication.class, args);
+	}
+
+}
