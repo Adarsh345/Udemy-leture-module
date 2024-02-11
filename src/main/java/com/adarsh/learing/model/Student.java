ae4a03b1cc39164bc2e2a7cf8333c3dceb091151
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-02-11 22:33:10 +0530

    1. user of session id 2. Understand CSRF attack and user of CSRF token 3. Get api for csrf token.

---
+++ src/main/java/com/adarsh/learing/model/Student.java
@@ -0,0 +1,13 @@
+package com.adarsh.learing.model;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private String tech;
+}
+
