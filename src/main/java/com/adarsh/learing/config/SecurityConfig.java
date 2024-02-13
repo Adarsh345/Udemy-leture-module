f7cbbc40e18f632b04c89cd6be24f7af6b527f51
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-02-14 00:22:27 +0530

    Get user from database and verify is it authenticated user.

--- src/main/java/com/adarsh/learing/config/SecurityConfig.java
+++ src/main/java/com/adarsh/learing/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.adarsh.learing.config;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,12 +13,28 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+
+        return daoAuthenticationProvider;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         //lambda configuration
@@ -26,7 +46,7 @@ public class SecurityConfig {
     }
 
 
-    @Bean
+  /*  @Bean
     public UserDetailsService userDetailsService(){
         UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("shivam")
@@ -40,5 +60,7 @@ public class SecurityConfig {
                 .roles("MANAGER")
                 .build();
         return new InMemoryUserDetailsManager(admin,manager);
-    }
+    }*/
+
+
 }
---
+++ src/main/java/com/adarsh/learing/service/UserService.java
@@ -0,0 +1,30 @@
+package com.adarsh.learing.service;
+
+import com.adarsh.learing.model.User;
+import com.adarsh.learing.repository.UserRepo;
+import com.adarsh.learing.securityImpl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user =  userRepo.findByUsername(username);
+        System.out.println(user);
+
+        if(user == null){
+            System.out.println("user 404");
+            throw  new UsernameNotFoundException("User not found ");
+        }
+
+        return new UserDetailsServiceImpl(user);
+
+    }
+}
