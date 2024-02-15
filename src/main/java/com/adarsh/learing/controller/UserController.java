bfed67295a5c39d0e30c701e0c0de1ac7c56b816
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-02-15 22:48:43 +0530

    use BCryptPasswordEncoder without using creating bean of encoder.

--- src/main/java/com/adarsh/learing/controller/UserController.java
+++ src/main/java/com/adarsh/learing/controller/UserController.java
@@ -3,6 +3,7 @@ package com.adarsh.learing.controller;
 import com.adarsh.learing.model.User;
 import com.adarsh.learing.securityImpl.UserServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,8 +12,12 @@ public class UserController {
     @Autowired
     private UserServiceInterface userService;
 
+    private BCryptPasswordEncoder cryptPasswordEncoder = new BCryptPasswordEncoder(12);
+
     @PostMapping(path = "/register")
     public User userRegister(@RequestBody User user){
+        user.setPassword(cryptPasswordEncoder.encode(user.getPassword()));
+        System.out.println(user.getPassword());
         return  userService.registerUser(user);
 
 
